a405ad86917a7d349fbfa549874ecfd1010a281c
Jackson Murphy <jackmurphy687@gmail.com>
2025-01-11 23:23:33 -0500

    add file for neural network and comments for what to do

---
+++ brain.js
@@ -0,0 +1,19 @@
+// neural network class represents all layers
+class NeuralNetwork {
+    // constructor (input neurons for each layer)
+    
+    // feed forward (pass outputs between layers)
+
+    // mutate function 
+
+}
+
+// class to represent each level
+class Level {
+    // constructor (take input (position? height?) and output counts (control options = 2))
+
+    // method to randomize weights and biases
+
+    // method to perform feed forward
+
+}
